8c8e5fa17e0bfa6db15543691f6d24d8281120a7
martimsoutooo <santos.martim@ua.pt>
2024-05-31 00:18:38 +0100

    i

--- docs/store_procedures/createUser.sql
+++ docs/store_procedures/createUser.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE CreateUser
+CREATE OR ALTER PROCEDURE CreateUser
     @Name NVARCHAR(50),
     @Email NVARCHAR(100),
     @Password NVARCHAR(100)
 AS
 BEGIN
-    INSERT INTO LCM.[User] (Name, Email, Password)
-    VALUES (@Name, @Email, dbo.HashPassword(@Password));
+    INSERT INTO LCM.[User] (Name, Email, Password,BE)
+    VALUES (@Name, @Email, dbo.HashPassword(@Password), 5000);
 END;
