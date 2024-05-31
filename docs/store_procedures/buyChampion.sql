cac5c2b2f9960e3cc29ece207063fb1676ef0cc5
martimsoutooo <santos.martim@ua.pt>
2024-05-31 02:00:08 +0100

    procedure buyChampion com transitions

--- docs/store_procedures/buyChampion.sql
+++ docs/store_procedures/buyChampion.sql
@@ -1,24 +1,34 @@
-CREATE PROCEDURE BuyChampion
+CREATE OR ALTER PROCEDURE BuyChampion
     @UserID INT,
     @ChampionID INT,
     @BECost INT
 AS
 BEGIN
     SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
 
-    DECLARE @UserBE INT;
-    SELECT @UserBE = BE FROM LCM.[User] WHERE ID = @UserID;
+        DECLARE @UserBE INT;
+        SELECT @UserBE = BE FROM LCM.[User] WHERE ID = @UserID;
 
-    IF @UserBE >= @BECost
-    BEGIN
-        UPDATE LCM.[User] SET BE = BE - @BECost WHERE ID = @UserID;
-        INSERT INTO LCM.User_Item (ID_User, ID_Item, Data, Hora)
-        VALUES (@UserID, @ChampionID, GETDATE(), CONVERT(VARCHAR, GETDATE(), 108)); -- Corrige a inserção de Data e Hora
+        IF @UserBE >= @BECost
+        BEGIN
+            UPDATE LCM.[User] SET BE = BE - @BECost WHERE ID = @UserID;
+            INSERT INTO LCM.User_Item (ID_User, ID_Item, Data, Hora)
+            VALUES (@UserID, @ChampionID, GETDATE(), CONVERT(VARCHAR, GETDATE(), 108));
 
-        SELECT 'Success' AS Result, 'Champion purchased successfully' AS Message;
-    END
-    ELSE
-    BEGIN
-        SELECT 'Error' AS Result, 'Not enough Blue Essence' AS Message;
-    END
+            COMMIT TRANSACTION;
+            SELECT 'Success' AS Result, 'Champion purchased successfully' AS Message;
+        END
+        ELSE
+        BEGIN
+            ROLLBACK TRANSACTION;
+            SELECT 'Error' AS Result, 'Not enough Blue Essence' AS Message;
+        END
+
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        SELECT 'Error' AS Result, ERROR_MESSAGE() AS Message;
+    END CATCH
 END;
